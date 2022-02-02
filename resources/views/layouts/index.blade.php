b24073af8fbf05563c85aceedec29d123d6dab41
flandy12 <halloflandy@gmail.com>
2022-02-02 10:28:35 +0700

    Update 02 FEBUARI - Delete tinymce script

--- resources/views/layouts/index.blade.php
+++ resources/views/layouts/index.blade.php
@@ -19,8 +19,7 @@
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
-    <script src="https://cdn.tiny.cloud/1/no-api-key/tinymce/5/tinymce.min.js" referrerpolicy="origin"></script>
-    <script src="{{ asset('js/tinymce.js') }}" defer></script>
+   
 </head>
 
 <body class="font-sans antialiased">
@@ -49,6 +48,8 @@
     @stack('modals')
 
     @livewireScripts
+    <script src="https://cdn.tiny.cloud/1/no-api-key/tinymce/5/tinymce.min.js" referrerpolicy="origin"></script>
+    <script src="{{ asset('js/tinymce.js') }}" defer></script>
 </body>
 
 </html>
